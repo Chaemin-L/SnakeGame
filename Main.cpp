3b75a93074175374da1b526a6f3d0604a351ed3e
chaem <chaemin12@kookmin.ac.kr>
2021-05-13 19:07:35 +0900

    키패드 입력값 안보이도록

--- Main.cpp
+++ Main.cpp
@@ -1,46 +1,14 @@
 #include <ncurses.h>
 #include <iostream>
 #include "Rules.cpp"
-/*
-class Snake{
-protected:
-  int y, x;
-  const char *ch;
-public:
-  Snake(int yp = 10, int xp = 30, const char *c = nullptr):  y(yp), x(xp), ch(c){}
-  int getY(){return y;}
-  int getX(){return x;}
 
-  int setY(int i){return y+i;}
-  int setX(int i){return x+i;}
-  void setposition(int i, int j){ mvprintw(y+=i, x+=j, ch); }
-};
-
-Body(){
-  for(int i=0; i<len; i++)
-    bd[i] = new int[MAXLEN];
-  bd[0][0] = bd[1][0] = 10;
-  bd[0][1] = 31; bd[1][1] = 32;
-  const char c = '4';
-  ch = &c;
-  show();
-}
-
-~Body(){
-  for(int i=0; i<MAXLEN; i++){
-    delete[] bd[i];
-  }
-  delete[] bd;
-}
-
-
-*/
 
 
 int main()
 {
   initscr();
   curs_set(0);  // invisible cursor
+  noecho();     // invisible keypad input
   start_color();
   init_pair(1, COLOR_BLUE, COLOR_YELLOW); // background color
   init_pair(2, COLOR_RED, COLOR_GREEN); // wall color
